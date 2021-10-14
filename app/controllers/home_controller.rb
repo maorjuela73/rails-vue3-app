10d90e8e970e26ce4807601cd3e42a726e09e55d
Miguel Orjuela <ma.orjuela73@gmail.com>
2021-10-14 18:11:29 -0500

    Listo para trabajar

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  def index
+    puts "akjgsdakldhjlakhlajkaljkasjkhasdjkhadk"
+    puts "akjgsdakldhjlakhlajkaljkasjkhasdjkhadk"
+    puts "akjgsdakldhjlakhlajkaljkasjkhasdjkhadk"
+    puts "akjgsdakldhjlakhlajkaljkasjkhasdjkhadk"
+    puts "akjgsdakldhjlakhlajkaljkasjkhasdjkhadk"
+  end
+end
